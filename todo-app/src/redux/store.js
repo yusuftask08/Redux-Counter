f212dceb18ef74f5c850cf5e0bd318937cc064d9
yusuftask08 <52704572+yusuftask08@users.noreply.github.com>
2022-03-07 19:31:47 +0300

    todo app deleted

--- todo-app/src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import todosSlice from './todos/todosSlice'
-export const store = configureStore({
-    reducer: {
-        todos: todosSlice
-    }
-})

3e7bd4d3ef7102e9ec442cc61c3b42e9c60f863a
yusuftask08 <52704572+yusuftask08@users.noreply.github.com>
2022-03-07 18:44:54 +0300

    Filtering Operations

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { toogle, deleteTodo } from '../redux/todos/todosSlice'
+let filtered = [];
 function TodoList() {
-    const items = useSelector(state => state.todos.items)
     const dispatch = useDispatch();
+    const items = useSelector(state => state.todos.items)
+    const activeFilter = useSelector(state => state.todos.activeFilter)
     const handleDestroy = (id) => {
-
         if (window.confirm('Are you sure ?')) {
             dispatch(deleteTodo(id))
         }
+    }
+    filtered = items;
+    if (activeFilter !== 'all') {
+        filtered = items.filter((todo) => activeFilter === 'active' ? todo.completed === false && todo : todo.completed === true && todo)
     }
     return (
         <div>
             <ul className="todo-list">
                 {
-                    items.map((item) => (
+                    filtered.map((item) => (
                         <li key={item.id} className={item.completed ? 'completed' : ''}>
                             <div className="view">
                                 <input
--- todo-app/src/redux/todos/todosSlice.js
+++ todo-app/src/redux/todos/todosSlice.js
@@ -15,6 +15,7 @@ export const todosSlice = createSlice({
                 completed: false
             }
         ],
+        activeFilter: 'all'
     },
     reducers: {
         addTodo: (state, action) => {
@@ -29,9 +30,17 @@ export const todosSlice = createSlice({
             const id = action.payload;
             const filtered = state.items.filter((item) => item.id !== id);
             state.items = filtered;
+        },
+        changeActiveFilter: (state, action) => {
+            console.log('action', action)
+            state.activeFilter = action.payload;
+        },
+        clearCompleted: (state) => {
+            const filter = state.items.filter(item => item.completed === false)
+            state.items = filter;
         }
     }
 })
 
-export const { addTodo, toogle, deleteTodo } = todosSlice.actions
+export const { addTodo, toogle, deleteTodo, changeActiveFilter, clearCompleted } = todosSlice.actions
 export default todosSlice.reducer;
